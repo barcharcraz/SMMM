a0837a164fd0c84bfd63a8bfcd3a837295bf214a
Dylan <dylanrb123@gmail.com>
2012-01-06 14:01:39 -0500

    minecraft instance no errors(that we know of)

--- SMMMLib/MinecraftInstance.cs
+++ SMMMLib/MinecraftInstance.cs
@@ -7,14 +7,14 @@ namespace SMMMLib
 {
     public class MinecraftInstance
     {
-        private Paths m_path;
+        private MinecraftPaths m_path;
         private MinecraftJar m_jar;
         private ModConfig m_config;
         public MinecraftInstance()
         {
-            m_path = new Paths();
+            m_path = new MinecraftPaths();
             m_jar = new MinecraftJar(m_path.jarPath);
-            m_config = new ModConfig(
+            m_config = new ModConfig(m_path);
         }
         
         
