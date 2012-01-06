927bb144795a03df7142a27ef01dce1d146b71e6
Dylan <dylanrb123@gmail.com>
2012-01-06 13:59:58 -0500

    MC instance starting point

---
+++ SMMMLib/MinecraftInstance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SMMMLib
+{
+    public class MinecraftInstance
+    {
+        private Paths m_path;
+        private MinecraftJar m_jar;
+        private ModConfig m_config;
+        public MinecraftInstance()
+        {
+            m_path = new Paths();
+            m_jar = new MinecraftJar(m_path.jarPath);
+            m_config = new ModConfig(
+        }
+        
+        
+    }
+}
