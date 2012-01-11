51a21a1e205233ff32ae06eac259807a744ab6b1
Charlie Barto <barcharcraz@gmail.com>
2012-01-10 21:02:05 -0500

    Started work on the GUI

--- SMMMWPF/MainWindow.xaml.cs
+++ SMMMWPF/MainWindow.xaml.cs
@@ -11,16 +11,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SMMMLib;
 
 namespace SMMMWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    
     public partial class MainWindow : Window
     {
+        MinecraftInstance instance;
+        MinecraftPaths m_paths;
         public MainWindow()
         {
+            m_paths = new MinecraftPaths();
+            XmlDataProvider dpro = new XmlDataProvider();
+            Uri furi = new Uri(System.IO.Path.Combine(m_paths.appConfigDir));
+
+            dpro.Source = furi;
+            
+            dpro.XPath = "SMMMconfig";
+            Binding bind = new Binding();
+            bind.Source = dpro;
+            ModsBox.SetBinding(ListBox.ItemsSourceProperty, bind);
             InitializeComponent();
         }
     }
